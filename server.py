bd41151a2d44b2831c0d9fd6a0433fee6f506875
Ayushkr3 <swdeepkumar2016.bth@gmail.com>
2021-05-05 16:32:20 +0000

    no des

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 s.bind(('localhost', 7777))
 s.listen(10)
-while True:
+conn,ca = s.accept()
+print("conn by ",ca)
+with conn:
     
-    conn,ca = s.accept()
-    print("conn by ",ca)
     conn.recv(1024)
